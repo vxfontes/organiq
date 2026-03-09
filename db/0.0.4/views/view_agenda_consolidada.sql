61e98cdf7a6074db1ffe359e3c215fbb74c4d785
vxfontes <nessa1vane@icloud.com>
2026-03-08 21:28:18 -0300

    - db - ajustes de query

--- db/0.0.4/views/view_agenda_consolidada.sql
+++ db/0.0.4/views/view_agenda_consolidada.sql
@@ -3,7 +3,7 @@ SELECT
     'task' as item_type,
     t.id, t.user_id, t.title, t.status, t.due_at as scheduled_at,
     f.name as flag_name, f.color as flag_color,
-    sf.name as subflag_name, sf.color as subflag_color
+    sf.name as subflag_name, f.color as subflag_color
 FROM inbota.tasks t
 LEFT JOIN inbota.flags f ON t.flag_id = f.id
 LEFT JOIN inbota.subflags sf ON t.subflag_id = sf.id
@@ -13,7 +13,7 @@ SELECT
     'reminder' as item_type,
     r.id, r.user_id, r.title, r.status, r.remind_at as scheduled_at,
     f.name as flag_name, f.color as flag_color,
-    sf.name as subflag_name, sf.color as subflag_color
+    sf.name as subflag_name, f.color as subflag_color
 FROM inbota.reminders r
 LEFT JOIN inbota.flags f ON r.flag_id = f.id
 LEFT JOIN inbota.subflags sf ON r.subflag_id = sf.id
@@ -23,7 +23,7 @@ SELECT
     'event' as item_type,
     e.id, e.user_id, e.title, 'OPEN' as status, e.start_at as scheduled_at,
     f.name as flag_name, f.color as flag_color,
-    sf.name as subflag_name, sf.color as subflag_color
+    sf.name as subflag_name, f.color as subflag_color
 FROM inbota.events e
 LEFT JOIN inbota.flags f ON e.flag_id = f.id
 LEFT JOIN inbota.subflags sf ON e.subflag_id = sf.id
