d1f6c60b170392ca3f96ab774590bb162156bb55
vxfontes <nessa1vane@icloud.com>
2026-03-22 15:27:35 -0300

    chore: simplify OQSkeleton to avoid per-instance animation controllers

--- app/lib/shared/components/oq_lib/oq_skeleton.dart
+++ app/lib/shared/components/oq_lib/oq_skeleton.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:organiq/shared/theme/app_colors.dart';
 
-class OQSkeleton extends StatefulWidget {
+class OQSkeleton extends StatelessWidget {
   const OQSkeleton({
     super.key,
     this.width,
@@ -16,49 +16,15 @@ class OQSkeleton extends StatefulWidget {
   final EdgeInsetsGeometry? margin;
 
   @override
-  State<OQSkeleton> createState() => _OQSkeletonState();
-}
-
-class _OQSkeletonState extends State<OQSkeleton>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1100),
-    )..repeat(reverse: true);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, _) {
-        final color = Color.lerp(
-          AppColors.surface2,
-          AppColors.border,
-          _controller.value,
-        )!;
-
-        return Container(
-          width: widget.width,
-          height: widget.height,
-          margin: widget.margin,
-          decoration: BoxDecoration(
-            color: color,
-            borderRadius: BorderRadius.circular(widget.radius),
-          ),
-        );
-      },
+    return Container(
+      width: width,
+      height: height,
+      margin: margin,
+      decoration: BoxDecoration(
+        color: AppColors.surface2,
+        borderRadius: BorderRadius.circular(radius),
+      ),
     );
   }
 }
